818040b7699c522bd5148024c3074dfbc0780a5b
vbolinches <vbolinches@alfatecsistemas.es>
2021-09-18 11:57:45 +0200

    Refactors Asyc Test Documentation

--- src/main/java/spring/api/asyn/email/EmailApplication.java
+++ src/main/java/spring/api/asyn/email/EmailApplication.java
@@ -1,4 +1,4 @@
-package jeferagudeloc.api.email;
+package spring.api.asyn.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
